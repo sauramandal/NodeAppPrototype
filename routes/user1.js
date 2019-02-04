123a8cdb51b0f6ae6dcb6547d4c9a3588fbf6d12
Montai <saura.mandal1@gmail.com>
2019-02-04 17:00:28 +0530

    used express router for handling specific url patterns

--- routes/user1.js
+++ routes/user1.js
@@ -1,8 +1,23 @@
 const router = require('express').Router();
 const {signUpValidation, loginValidation } =  require('../utility/validation');
+const UserService = require('../services/UserService');
 
 router.get('/all', (req, res) => {
   // code for getting all users
+  console.log(req.body);
+  UserService
+  	.showAllUsers()
+  	.then((res) => {
+  		return res.json({
+  			message: "users successfully fetched",
+  			result: res
+  		});
+  	})
+  	.catch((err) => {
+  		return res.json({
+  			message: "unable to fetch users"
+  		});
+  	});
   // send response
 });
 
