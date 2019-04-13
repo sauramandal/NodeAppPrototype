e1abe281fc9a38c97ecd38788dc5021f8c5e29bb
Montai <saura.mandal1@gmail.com>
2019-04-13 14:35:37 +0530

    Added an Api endpoint for viewing users todays orders: /orders/:id/showTodaysOrder

--- routes/order.js
+++ routes/order.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const OrderService = require('../services/OrderService');
 
 router.get('/orders', OrderService.allOrders);
-
+//viewing my todays orders
+router.get('/:id/showTodaysOrder', OrderService.getTodayOrdersForCurrentUser);
 module.exports = router;
--- services/OrderService.js
+++ services/OrderService.js
@@ -1,4 +1,5 @@
 const {db}  = require('./../db');
+const sequelize = db.sequelize;
 
 exports.allOrders = (req, res) => {
   db.tb_order.findAll()
@@ -9,4 +10,31 @@ exports.allOrders = (req, res) => {
       console.log(error);
       res.status(404).send(error);
     })
+}
+
+exports.getTodayOrdersForCurrentUser = (req, res) => {
+  let userId = req.params.id;
+  let today = new Date().toISOString().split('T')[0];
+  sequelize.query('SELECT TP.product_name, TP.product_price, TOD.QUANTITY\
+                  FROM tb_order TORD\
+                  INNER JOIN tb_order_details TOD \
+                  ON TORD.ID = TOD.ORDER_ID\
+                  INNER JOIN tb_product TP ON TP.id = TOD.PRODUCT_ID\
+                  WHERE TORD.USER_ID = :usersId AND TORD.ORDER_DATE = :todaysDate', 
+                  { 
+                    replacements: 
+                    {
+                      usersId: userId,
+                      todaysDate: today
+                    }, 
+                    type: sequelize.QueryTypes.SELECT 
+                  }
+  )
+  .then(orders => {
+    res.json(orders);
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(404).send(error);
+  }); 
 }
