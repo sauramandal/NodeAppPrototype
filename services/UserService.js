a7edf4f2a9ad0bde2cfb8b64513d0c1d73477478
Pritimay Sarkar <pritimay.sarkar@ril.com>
2019-04-28 14:14:51 +0530

    remove regex validation for password while login

--- services/UserService.js
+++ services/UserService.js
@@ -68,7 +68,7 @@ module.exports = {
     return new Promise((resolve, reject) => {
       const schema = Joi.object({
         email: Joi.string().required(),
-        password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).min(6).required()
+        password: Joi.string().min(6).required()
     }).with('email', 'password');
 
       const login = {
