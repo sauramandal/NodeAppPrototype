b94e741b873a86d905ff85432de06e1c7cafd55f
Pritimay Sarkar <pritimay.sarkar@ril.com>
2019-04-28 18:11:40 +0530

    get my orders api

--- routes/api/order.js
+++ routes/api/order.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const OrderController = require('../../controllers/OrderController');
 
+router.get('/myorders?:customerid', OrderController.getOrdersByCustomerId);
+
 router.get('/:id', OrderController.getOrderById);
 
 router.post('/', OrderController.addOrder);
